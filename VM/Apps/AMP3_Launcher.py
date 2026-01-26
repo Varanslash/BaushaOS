5ab76eaed0363db9b0a9e71c07efa56bff1e2d12
Varanslash <odiaenvy@gmail.com>
2026-01-26 15:52:27 -0600

    Update AMP3_Launcher.py

--- VM/Apps/AMP3_Launcher.py
+++ VM/Apps/AMP3_Launcher.py
@@ -1,3 +1,4 @@
 import subprocess
-subprocess.run(["powershell", "-Command", "powershell.exe -File C:\\Users\\odiae\\Downloads\\VarOS\\Userspace\\Apps\\AMP3_Player\\AMP3_Player.ps1"])
+subprocess.run(["powershell", "-Command", "powershell.exe -File C:\\Users\\$env:USERNAME\\Downloads\\VarOS\\Userspace\\Apps\\AMP3_Player\\AMP3_Player.ps1"])
+
 exit()
