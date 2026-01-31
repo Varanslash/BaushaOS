500bb7ff9fa3d087487baefbf76ddab83c145e12
Varanslash <odiaenvy@gmail.com>
2026-01-30 18:15:27 -0600

    hardware: add first MicroAqua command
    "echo" added

--- Hardware/main.py
+++ Hardware/main.py
@@ -84,7 +84,13 @@ except TypeError:
 
 # /// Aquamarine Language Compiler ///
 
-
+def interpret(lines):
+    lines = lines.split("\n")
+    indent -= 1
+    for line in lines:
+        line = line.split()
+        if line[0] == "echo":
+            print(line[1:])
 
 # /// Vortex Markov Chain Bot ///
 
@@ -210,3 +216,4 @@ while True:
         dragon["happiness"] += random.randint(1, 5)
     except IndexError:
         errorcode(1)
+
